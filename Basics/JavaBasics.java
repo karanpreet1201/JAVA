7022debc13c181564dd9088d8a8aba5d6bce9bbf
Karanpreet Singh <karanpreetsingh78668@gmail.com>
2024-07-18 11:02:48 +0530

    Create JavaBasics.java

---
+++ Basics/JavaBasics.java
@@ -0,0 +1,30 @@
+public class JavaBasics{
+    public static void main(String args[]){
+        // System.out.println("Hello World");
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+
+        // byte b = 8;
+        // System.out.println(b);
+        // char ch = 'a';
+        // System.out.println(ch);
+        // boolean var = false;
+        // float price = 0.5f;
+        // System.out.println(price);
+        // int number = 25;
+        // long 
+        // double
+
+
+
+
+
+        // int a = 10;
+        // int b = 5;
+        // int sum = a + b;
+        // System.out.println(sum);
+        
+    }
+}
