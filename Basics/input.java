c5e3736c3e59cd33d6bb6617ebdb78fe4236860b
Karanpreet Singh <karanpreetsingh78668@gmail.com>
2024-07-18 11:02:43 +0530

    Create input.java

---
+++ Basics/input.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class input {
+    public static void main(String args[]){
+
+        // Single word Before space :- karanpreet Singh -> Karanpreet
+
+        // Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+        // System.out.println(input);
+
+        // Karanpreet Singh -> Karanpreet Singh
+
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+
+        // Integer 
+
+        // Scanner sc = new Scanner(System.in);
+        // int number = sc.nextInt();
+        // System.out.println(number);
+
+
+        // Float
+
+        Scanner sc = new Scanner(System.in);
+        float price = sc.nextFloat();
+        System.out.println(price);
+        sc.close();
+    }
+}
