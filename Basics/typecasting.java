ccf6453d5015c5a6d7bddb088363f69284ca2f2a
Karanpreet Singh <karanpreetsingh78668@gmail.com>
2024-07-22 09:48:30 +0530

    Create typecasting.java

---
+++ Basics/typecasting.java
@@ -0,0 +1,14 @@
+// import java.util.*;
+public class typecasting {
+    public static void main(String args[]){
+        
+        float number1 = 99.999f;
+        int number2 = (int) number1;
+        System.out.println(number2);
+
+
+        // char ch = 'a';
+        // int number = ch;
+        // System.out.println(number);
+    }
+}
