4fe5fadeeea8239a8831e50e0e627e0989998060
Karanpreet Singh <karanpreetsingh78668@gmail.com>
2024-07-23 13:59:41 +0530

    Create typepromotion.java

---
+++ Basics/typepromotion.java
@@ -0,0 +1,22 @@
+public class typepromotion {
+    public static void main(String args[]){
+        
+
+        short a = 5;
+        byte b = 25;
+        char c = 'c';
+        byte bt = (byte) (a + b + c);
+        System.out.println(bt);
+
+        
+        
+        
+        
+        // char a = 'a';
+        // char b = 'b';
+        // System.out.println((int)a);
+        // System.out.println((int)b);
+        // System.out.println(b - a);
+    }
+    
+}
