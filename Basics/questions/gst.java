54a48c41c71115e46735fbfbcfe3922dcca22efe
Karanpreet Singh <karanpreetsingh78668@gmail.com>
2024-08-15 11:55:49 +0530

    Calculate GST on Product

---
+++ Basics/questions/gst.java
@@ -0,0 +1,21 @@
+package questions;
+
+import java.util.Scanner;
+
+public class gst 
+{
+    public static void main(String args[])
+    {   Scanner sc= new Scanner(System.in);
+        float pencil =sc.nextFloat();
+        float pen = sc.nextFloat();
+        float eraser = sc.nextFloat();
+        float total = pencil + pen + eraser;
+        System.out.println("Bill is : "+total);
+        
+        //Add on - with 18% tax 
+        float newTotal=total+ (0.18f*total);
+        System.out.println("Bill with 18% tax : "+newTotal);
+
+        sc.close();
+    }
+}
