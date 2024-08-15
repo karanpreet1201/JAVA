d3450b6796bc8dfef5597300208d9d79a346af14
Karanpreet Singh <karanpreetsingh78668@gmail.com>
2024-08-15 11:10:20 +0530

    Update typrepromotion file

--- Basics/typepromotion.java
+++ Basics/typepromotion.java
@@ -2,11 +2,28 @@ public class typepromotion {
     public static void main(String args[]){
         
 
-        short a = 5;
-        byte b = 25;
-        char c = 'c';
-        byte bt = (byte) (a + b + c);
-        System.out.println(bt);
+        byte a = 5;
+        byte b = (byte) (a * 2);
+        System.out.println(b);
+
+
+
+        // int a = 10;
+        // float b = 20.25f;
+        // long c = 25;
+        // double d = 30;
+        // double ans = a + b + c + d;
+        // System.out.println(ans);
+
+
+
+
+
+        // short a = 5;
+        // byte b = 25;
+        // char c = 'c';
+        // byte bt = (byte) (a + b + c);
+        // System.out.println(bt);
 
         
         
