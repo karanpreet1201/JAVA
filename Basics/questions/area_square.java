8c41a724f32531aa3be66f21f9bc9e3d21f5d2c2
Karanpreet Singh <karanpreetsingh78668@gmail.com>
2024-08-15 11:41:56 +0530

    Area of a Square

---
+++ Basics/questions/area_square.java
@@ -0,0 +1,17 @@
+package questions;
+import java.util.*;
+
+public class area_square {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int side = sc.nextInt();
+
+        int area = side * side;
+        System.out.println(area);
+
+
+
+        sc.close();
+    }
+}
