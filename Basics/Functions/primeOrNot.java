2661293f8a57e2c9401efdbe37a25782eecb8460
Karanpreet Singh <karanpreetsingh78668@gmail.com>
2024-09-14 13:27:10 +0530

    Prime or not prime

---
+++ Basics/Functions/primeOrNot.java
@@ -0,0 +1,25 @@
+
+public class primeOrNot {
+    public static void main(String[] args) {
+        System.out.println(isPrime(7));
+    }
+
+
+    public static boolean isPrime(int n)
+    {
+        // Special case
+        if (n == 2)
+        {
+            return true;
+        }
+        
+        for (int i = 2 ; i <= Math.sqrt(n) ; i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
